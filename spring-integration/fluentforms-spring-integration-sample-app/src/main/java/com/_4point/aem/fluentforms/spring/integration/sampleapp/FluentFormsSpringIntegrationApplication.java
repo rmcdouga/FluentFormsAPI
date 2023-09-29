404d218cc2335d3f7af1b47249063e612899feca
Rob McDougall <rmcdougawww@rogers.com>
2023-09-29 07:59:48 -0400

    Updated with toy integrtion flow.

--- spring-integration/fluentforms-spring-integration-sample-app/src/main/java/com/_4point/aem/fluentforms/spring/integration/sampleapp/FluentFormsSpringIntegrationApplication.java
+++ spring-integration/fluentforms-spring-integration-sample-app/src/main/java/com/_4point/aem/fluentforms/spring/integration/sampleapp/FluentFormsSpringIntegrationApplication.java
@@ -4,17 +4,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.core.GenericHandler;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.messaging.support.MessageBuilder;
 
 @SpringBootApplication
 public class FluentFormsSpringIntegrationApplication {
 	private final static Logger log = LoggerFactory.getLogger(FluentFormsSpringIntegrationApplication.class);
 
 	public FluentFormsSpringIntegrationApplication(GitConfig gitConfig) {
-    	gitConfig.logGitInformation();
+		gitConfig.logGitInformation();
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FluentFormsSpringIntegrationApplication.class, args);
+	}
+
+	@Bean
+	IntegrationFlow flow() {
+		return IntegrationFlow.from((MessageSource<String>) () -> MessageBuilder.withPayload("testString").build())
+				.handle((GenericHandler<String>) (payload, headers) -> {
+					FluentFormsSpringIntegrationApplication.log.atInfo().addArgument(payload).log("Found String '{}'.");
+					return null;
+				}).get();
 	}
 
 }
