0333e9bc012aea8321a880deaf9e1dba31489142
Rob McDougall <rmcdougawww@rogers.com>
2023-09-30 07:56:56 -0400

    Got HTTP Request working...

--- spring-integration/fluentforms-spring-integration-sample-app/src/main/java/com/_4point/aem/fluentforms/spring/integration/sampleapp/FluentFormsSpringIntegrationApplication.java
+++ spring-integration/fluentforms-spring-integration-sample-app/src/main/java/com/_4point/aem/fluentforms/spring/integration/sampleapp/FluentFormsSpringIntegrationApplication.java
@@ -5,9 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.integration.core.GenericHandler;
 import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.http.dsl.Http;
+import org.springframework.integration.http.dsl.HttpRequestHandlerEndpointSpec;
 import org.springframework.messaging.support.MessageBuilder;
 
 @SpringBootApplication
@@ -24,11 +27,25 @@ public class FluentFormsSpringIntegrationApplication {
 
 	@Bean
 	IntegrationFlow flow() {
-		return IntegrationFlow.from((MessageSource<String>) () -> MessageBuilder.withPayload("testString").build())
-				.handle((GenericHandler<String>) (payload, headers) -> {
-					FluentFormsSpringIntegrationApplication.log.atInfo().addArgument(payload).log("Found String '{}'.");
-					return null;
-				}).get();
+		return IntegrationFlow.from(httpMessageSource())
+				.handle(handler()).get();
+	}
+
+	private GenericHandler<String> handler() {
+		return (GenericHandler<String>) (payload, headers) -> {
+			FluentFormsSpringIntegrationApplication.log.atInfo().addArgument(payload).log("Found String '{}'.");
+			return "Response";
+		};
+	}
+
+	private HttpRequestHandlerEndpointSpec httpMessageSource() {
+		return Http.inboundGateway("/service/test")
+				   .requestMapping(r->r.methods(HttpMethod.POST))
+				   ;
+	}
+
+	private MessageSource<String> stringMessageSource() {
+		return (MessageSource<String>) () -> MessageBuilder.withPayload("testString").build();
 	}
 
 }
--- spring-integration/fluentforms-spring-integration-sample-app/src/test/java/com/_4point/aem/fluentforms/spring/integration/sampleapp/FluentFormsSpringIntegrationApplicationTest.java
+++ spring-integration/fluentforms-spring-integration-sample-app/src/test/java/com/_4point/aem/fluentforms/spring/integration/sampleapp/FluentFormsSpringIntegrationApplicationTest.java
@@ -1,19 +1,38 @@
 package com._4point.aem.fluentforms.spring.integration.sampleapp;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest(webEnvironment = WebEnvironment.NONE, 
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, 
 				classes = FluentFormsSpringIntegrationApplication.class, 
 				properties = {"fluentforms.aem.servername=localhost", "fluentforms.aem.port=4502",
 							  "fluentforms.aem.user=user", "fluentforms.aem.password=password"})
 class FluentFormsSpringIntegrationApplicationTest {
 
+	@LocalServerPort
+	int port;
+	
+	
 	@Test
     public void whenSpringContextIsBootstrapped_thenNoExceptions() {
     }
 
+	@Test
+	public void whenHttpRequestArrives_getResponse() {
+		given()
+			.baseUri("http://localhost:" + port)
+			.body("Some Body Text")
+		.when()
+			.post("/service/test")
+		.then()
+			.statusCode(200)
+			.body(containsString("Response"));	
+	}
 }
