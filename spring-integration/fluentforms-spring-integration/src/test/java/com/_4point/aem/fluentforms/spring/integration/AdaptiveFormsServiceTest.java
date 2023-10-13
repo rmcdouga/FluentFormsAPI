2d5d26bf662416ed7f3aa11603ba2c413d91e70f
Rob McDougall <rmcdougawww@rogers.com>
2023-10-13 08:41:58 -0400

    Creating unit test (work in progress)

--- spring-integration/fluentforms-spring-integration/src/test/java/com/_4point/aem/fluentforms/spring/integration/AdaptiveFormsServiceTest.java
+++ spring-integration/fluentforms-spring-integration/src/test/java/com/_4point/aem/fluentforms/spring/integration/AdaptiveFormsServiceTest.java
@@ -2,15 +2,25 @@ package com._4point.aem.fluentforms.spring.integration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.core.GenericTransformer;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import com._4point.aem.docservices.rest_services.client.af.AdaptiveFormsService.AdaptiveFormsServiceException;
@@ -19,6 +29,7 @@ import com._4point.aem.fluentforms.api.Document;
 @ExtendWith(MockitoExtension.class)
 @SpringJUnitConfig(AdaptiveFormsServiceTest.TestConfiguration.class)
 class AdaptiveFormsServiceTest {
+	private static final String FORM_STR = "test/form1";
 
 	@Autowired
 	com._4point.aem.docservices.rest_services.client.af.AdaptiveFormsService ffAfService;
@@ -26,16 +37,39 @@ class AdaptiveFormsServiceTest {
 	@Autowired
 	private AdaptiveFormsService underTest;
 
-	@Mock
-	private Document mockDoc;
+	@Mock private Document mockDoc;
+	@Captor ArgumentCaptor<String> ffAfServiceForm;
 
+	private enum TestScenario {
+		renderBlankAdaptiveFormFromString(
+				()->MessageBuilder.withPayload(FORM_STR).build(),
+//				(Message<String> m)->AdaptiveFormsService.renderBlankAdaptiveFormFromString(m),
+				m->AdaptiveFormsService.renderBlankAdaptiveFormFromString((Message<String>)m),
+				m->{}
+				)
+		;
+		final Supplier<Message<?>> input;
+		final GenericTransformer<Message<?>, Message<?>> operationUnderTest;
+		final Consumer<Message<?>> validation;
+		
+		private TestScenario(Supplier<Message<?>> input, GenericTransformer<Message<?>, Message<?>> operationUnderTest,
+				Consumer<Message<?>> validation) {
+			this.input = input;
+			this.operationUnderTest = operationUnderTest;
+			this.validation = validation;
+		}
+
+	}
 	
-	@Test
-	void testRenderBlankAdaptiveForm() throws Exception {
+	@ParameterizedTest
+	@EnumSource
+	void testRenderBlankAdaptiveForm(TestScenario testScenario) throws Exception {
 		assertNotNull(underTest);
 		assertNotNull(ffAfService);
-		Mockito.when(ffAfService.renderAdaptiveForm(Mockito.anyString())).thenReturn(mockDoc);
-		
+		Mockito.when(ffAfService.renderAdaptiveForm(ffAfServiceForm.capture())).thenReturn(mockDoc);
+		Message<?> result = testScenario.operationUnderTest.transform(testScenario.input.get());
+		testScenario.validation.accept(result);
+//		ffAfServiceForm.getValue();
 	}
 
 	@Configuration
