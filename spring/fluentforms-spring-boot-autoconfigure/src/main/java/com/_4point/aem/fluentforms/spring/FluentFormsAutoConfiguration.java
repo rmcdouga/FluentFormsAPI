bb7f561186be3c2ea39318f79b7b094b0be24185
Rob McDougall <rmcdougawww@rogers.com>
2023-10-17 07:52:02 -0400

    Made AdaptiveFormsService conditional upon WebApplication
    It was defacto dependent upon bwing a Web Application because it
    requires beans that are conditional on Web Application, so I just made
    it explicit.

--- spring/fluentforms-spring-boot-autoconfigure/src/main/java/com/_4point/aem/fluentforms/spring/FluentFormsAutoConfiguration.java
+++ spring/fluentforms-spring-boot-autoconfigure/src/main/java/com/_4point/aem/fluentforms/spring/FluentFormsAutoConfiguration.java
@@ -5,6 +5,7 @@ import java.util.function.Function;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
@@ -58,6 +59,7 @@ public class FluentFormsAutoConfiguration {
 	}
 
 	@ConditionalOnMissingBean
+	@ConditionalOnWebApplication
 	@Bean
 	public AdaptiveFormsService adaptiveFormsService(AemConfiguration aemConfig, Function<InputStream, InputStream> afInputStreamFilter) {
 		return setAemFields(AdaptiveFormsService.builder(), aemConfig)
